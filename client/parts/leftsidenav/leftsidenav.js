48eab73f56c5f1eec3555bd20fa56f69d00c5a76
Aleksandr Popugaiev <aleksandr.popugaiev@gmail.com>
2018-11-22 11:35:48 +0200

    peer & socket works

--- client/parts/leftsidenav/leftsidenav.js
+++ client/parts/leftsidenav/leftsidenav.js
@@ -1,18 +1,74 @@
-import {Streams} from '../../collections/Streams'
+import Peer from 'simple-peer';
+import {
+  Streams
+} from '../../collections/Streams'
+import {
+  deviceId
+} from '../../js/deviceId'
 
-Template.leftsidenav.onCreated(function(){
+Template.leftsidenav.onCreated(function () {
   let t = this;
-  
+
 });
 
-Template.leftsidenav.onRendered(function(){
+Template.leftsidenav.onRendered(function () {
   let t = this;
   t.$('.collapsible').collapsible();
+
+  /*
+  const PORT = 8080;
+  let socket = require('socket.io-client')(`http://localhost:${PORT}`);
+
+  socket.on('connect', function () {
+    console.log('Connected to signalling server, self id : %s', socket.id);
+  });
+
+  let streams = Template.leftsidenav.__helpers.get('streams').call();
+
+  streams.forEach(stream => {
+    console.log("stream : ", stream);
+    if (stream.deviceId == deviceId) {
+      /// source pc
   
+      start_video(t.stream, stream.constraints, 'output').then((stream) => {
+        t.stream = stream;
+        let peer = new Peer({
+          initiator: true,
+          stream: stream
+        });
+  
+        peer.on('signal', function(data){
+          socket.emit('signal', {signal: data, id: socket.id, initiator: true});
+        });
+        socket.on('signal', function(data){
+          //peer.signal(data.signal)
+          console.log('initiator socket signal : ', data);
+        });
+      });
+  
+    } else {
+      // receiver pc
+      let peer = new Peer();
+  
+      peer.on('signal', function(data){
+        socket.emit('signal', {signal: data, peerId: socket.id});
+      });
+      
+      peer.on('stream', function(stream){
+        document.getElementById('output').srcObject = stream;
+      });
+  
+      socket.on('signal', function(data){
+        //peer.signal(data.signal)
+        console.log('receiver socket signal : ', data);
+      });
+    }
+  });
+  */
 });
 
 Template.leftsidenav.helpers({
-  streams(){
+  streams() {
     return Streams.find().fetch()
   }
 });
--- server/socket.js
+++ server/socket.js
@@ -8,35 +8,24 @@ Meteor.startup(() => {
   const server = http.createServer();
   const io = socket_io(server);
 
-  let counter = 0;
-
   // New client
   io.on('connection', function (socket) {
-    console.log('Connection with ID:', socket.id, Object.keys(io.sockets.connected));
-    Object.keys(io.sockets.connected).forEach(function (socketId) {
-      if (socketId != socket.id){
-        console.log('Advertising peer %s to %s', socket.id, sock.id);
-        io.sockets.connected[socketId].emit('peer', {
-          peerId: socket.id,
-          initiator: true
-        });
-        socket.emit('peer', {
-          peerId: socketId,
-          initiator: false
+    console.log('%s Peer Connected', socket.id);
+    console.log('connected sockets : ', Object.keys(io.sockets.connected));
+    Object.keys(io.sockets.connected).forEach((socketId) => {
+      if (socketId != socket.id) {
+        io.sockets.connected[socketId].emit('new_peer', {
+          id: socket.id
         });
       }
     });
 
     socket.on('signal', function (data) {
-      var sock = io.sockets.connected[data.peerId];
-      if (!sock) {
-        return;
-      }
-      console.log('Proxying signal from peer %s to %s', socket.id, sock.id);
-
-      sock.emit('signal', {
-        signal: data.signal,
-        peerId: socket.id
+      //console.log('server socket signal received : ', data);
+      Object.keys(io.sockets.connected).forEach((socketId) => {
+        if (socketId != socket.id) {
+          io.sockets.connected[socketId].emit('signal', data)
+        }
       });
     });
   });
