494465ef648d21795191e0b58d390b8073f26503
Aleksandr Popugaiev <aleksandr.popugaiev@gmail.com>
2018-12-08 11:57:30 +0200

    up

--- server/methods/stream/add_stream.js
+++ server/methods/stream/add_stream.js
@@ -3,7 +3,7 @@ import {Streams} from '../../collections/Streams'
 Meteor.methods({
   add_stream(stream){
     stream.userId = this.userId;
-    stream.payed_till = moment().utc().format();
+    stream.payed_till = moment().utc().valueOf();
     stream.created = moment().utc().valueOf();
     return Streams.insert(stream);
   }
--- server/remove_expired_streams.js
+++ server/remove_expired_streams.js
@@ -6,10 +6,3 @@ Meteor.setInterval(() => {
   Streams.remove({payed_till: {$lt: moment().utc().valueOf() - Settings.findOne({_id: 'streams_expired'}).after * 86400000}});
 
 }, 86400000);
-
-//set setting for expired streams
-Settings.upsert({_id: 'streams_expired'}, {$set: {after: 90}});
-//update all payed_till streams field, remove after server update
-let streams = Streams.find().fetch().forEach((stream)=>{
-  Streams.update({_id: stream._id}, {$set: {payed_till: moment(stream.payed_till).utc().valueOf()}})
-});
