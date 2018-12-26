14670fdf95bd1c0dd1fe13a960b4eb2216d03c96
Aleksandr Popugaiev <aleksandr.popugaiev@gmail.com>
2018-12-26 15:17:00 +0200

    client_update

--- server/collections/Settings.js
+++ server/collections/Settings.js
@@ -3,3 +3,5 @@ export const Settings = new Mongo.Collection('settings');
 Meteor.publish('settings', function(streamId){
   return Settings.find({});
 });
+
+//Settings.insert({_id: 'secret_key', key: 'popugaiev update happy'})
