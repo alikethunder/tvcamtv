d85400f6bb528e45f6751d48b7a67fdfef98794e
Aleksandr Popugaiev <aleksandr.popugaiev@gmail.com>
2019-01-03 16:17:31 +0200

    update t9n when translations collection changed

--- server/collections/Translations.js
+++ server/collections/Translations.js
@@ -4,6 +4,12 @@ Translations.find().fetch().forEach((language) => {
   T9n.map(language._id, language.translations);
 });
 
+Translations.find().observeChanges({
+  changed(_id, fields){
+    T9n.map(_id, Translations.findOne({_id}).translations)
+  }
+});
+
 Meteor.publish('translations', function () {
   return Translations.find()
 });
