596b0a49a81a5d716d83662465956f1830040553
Aleksandr Popugaiev <aleksandr.popugaiev@gmail.com>
2022-09-14 10:43:06 +0300

    update

--- server/httpsProxy.js
+++ server/httpsProxy.js
@@ -1,10 +1,11 @@
+import {readFileSync} from 'fs'
+import httpProxy from 'http-proxy'
+import {Settings} from './collections/Settings'
+
 const [,, host, port] = Meteor.absoluteUrl().match(/([a-zA-Z]+):\/\/([\-\w\.]+)(?:\:(\d{0,5}))?/);
 
 // if not developer session
 if (port != 3000){
-  import {readFileSync} from 'fs'
-  import httpProxy from 'http-proxy'
-  import {Settings} from './collections/Settings'
   
   let { key, cert } = Settings.findOne({_id: 'ssl_certificates'});
   httpProxy.createServer({
