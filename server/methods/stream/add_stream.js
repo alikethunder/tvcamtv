190ba2bdc59abfb9658ace9165495f7acc24a2f0
Aleksandr Popugaiev <aleksandr.popugaiev@gmail.com>
2022-09-20 17:16:34 +0300

    remove payed_till from addstream method

--- server/methods/stream/add_stream.js
+++ server/methods/stream/add_stream.js
@@ -3,8 +3,6 @@ import {Streams} from '../../collections/Streams'
 Meteor.methods({
   add_stream(stream){
     stream.userId = this.userId;
-    stream.payed_till = moment().utc().valueOf();
-    stream.created = moment().utc().valueOf();
     return Streams.insert(stream);
   }
 });
